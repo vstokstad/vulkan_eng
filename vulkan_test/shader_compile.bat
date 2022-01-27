73c5a42e19bac766d5ba7640d3f0f6b0a708b112
Vilhelm Stokstad <v.stokstad@gmail.com>
2022-01-27 23:44:17 +0100

    rmv solution files and added cmakelists and shader target in that. slight restructure of folders AGAIN... sorry

--- vulkan_test/shader_compile.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-echo %cd% 
-echo compiling shaders:
-forfiles /P shaders /S /M *.vert /C "cmd /c echo @file"
-forfiles /P shaders /S /M *.frag /C "cmd /c echo @file"
-forfiles /P shaders /S /M *.vert /C "cmd /c glslc -c @file"
-forfiles /P shaders /S /M *.frag /C "cmd /c glslc -c @file"
-echo Done! compiled files:
-forfiles /P shaders /D +0 /S /M *.spv /C "cmd /c echo @file"
